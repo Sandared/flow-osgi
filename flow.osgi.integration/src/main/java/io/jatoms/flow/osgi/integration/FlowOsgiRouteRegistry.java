ffe34d5256161a682c5b9ca93b5f6ae373d91272
Thomas Driessen <thomas.driessen.td@gmail.com>
2019-02-02 15:26:30 +0100

    Switched off source code generation in order to be able to properly debug the application within eclipse

--- flow.osgi.integration/src/main/java/io/jatoms/flow/osgi/integration/FlowOsgiRouteRegistry.java
+++ flow.osgi.integration/src/main/java/io/jatoms/flow/osgi/integration/FlowOsgiRouteRegistry.java
@@ -3,6 +3,7 @@ package io.jatoms.flow.osgi.integration;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 import com.vaadin.flow.server.startup.RouteRegistry;
@@ -11,7 +12,11 @@ import static io.jatoms.flow.osgi.integration.FlowOsgiConstants.*;
 @Component(service = RouteRegistry.class)
 public class FlowOsgiRouteRegistry extends RouteRegistry {
 	
-	@Reference(target="(" + Annotation + "=" + Route + ")", cardinality=ReferenceCardinality.MULTIPLE, policyOption=ReferencePolicyOption.GREEDY)
+	@Reference(
+			target="(" + Annotation + "=" + Route + ")", 
+			cardinality=ReferenceCardinality.MULTIPLE, 
+			policy=ReferencePolicy.DYNAMIC,
+			policyOption=ReferencePolicyOption.GREEDY)
 	void addRoute(Class route) {
 		String test = "";
 		// TODO: add route to navigation targets, maybe switch to vaadin 13 or snapshot version as 12 does not support dynamic routes?
@@ -21,7 +26,10 @@ public class FlowOsgiRouteRegistry extends RouteRegistry {
 		String test = "";
 	}
 	
-	@Reference(target="(" + Annotation + "=" + RouteAlias + ")", cardinality=ReferenceCardinality.MULTIPLE, policyOption=ReferencePolicyOption.GREEDY)
+	@Reference(target="(" + Annotation + "=" + RouteAlias + ")", 
+			cardinality=ReferenceCardinality.MULTIPLE,
+			policy=ReferencePolicy.DYNAMIC,
+			policyOption=ReferencePolicyOption.GREEDY)
 	void addRouteAlias(Class route) {
 		String test = "";
 		// TODO: add route to navigation targets, maybe switch to vaadin 13 or snapshot version as 12 does not support dynamic routes?
--- flow.osgi.integration/src/main/java/io/jatoms/flow/osgi/integration/FlowOsgiServletContextListener.java
+++ flow.osgi.integration/src/main/java/io/jatoms/flow/osgi/integration/FlowOsgiServletContextListener.java
@@ -1,10 +1,13 @@
 package io.jatoms.flow.osgi.integration;
 
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardListener;
 
@@ -16,15 +19,24 @@ public class FlowOsgiServletContextListener implements ServletContextListener {
 	
 	@Reference
 	RouteRegistry routeRegistry;
+	
+	private CopyOnWriteArrayList<ServletContext> contexts = new CopyOnWriteArrayList<ServletContext>();
+	
+	@Deactivate
+	void deactivate() {
+		contexts.stream()
+			.forEach(context -> context.removeAttribute(RouteRegistry.class.getName()));
+	}
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		sce.getServletContext().setAttribute(RouteRegistry.class.getName(), routeRegistry);
+		ServletContext context = sce.getServletContext();
+		context.setAttribute(RouteRegistry.class.getName(), routeRegistry);
+		contexts.add(context);
 	}
 
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
-		// TODO Auto-generated method stub
-
+		contexts.remove(sce.getServletContext());
 	}
 }
