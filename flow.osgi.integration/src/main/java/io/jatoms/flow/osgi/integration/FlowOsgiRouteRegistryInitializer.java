7c6555b6b0b3ae5cf53ec1d4227ba80e8c411658
Sandared <thomas.driessen.td@gmail.com>
2019-02-06 17:41:07 +0100

    Update FlowOsgiRouteRegistryInitializer.java

--- flow.osgi.integration/src/main/java/io/jatoms/flow/osgi/integration/FlowOsgiRouteRegistryInitializer.java
+++ flow.osgi.integration/src/main/java/io/jatoms/flow/osgi/integration/FlowOsgiRouteRegistryInitializer.java
@@ -17,8 +17,12 @@ import com.vaadin.flow.server.startup.RouteRegistry;
 // Somehow this does not work and the SCL is never called by the HTTP Whiteboard Service. 
 // I've already asked on the OSGi mailing list, but not received an answer yet 
 
-@Component(immediate=true)
-@HttpWhiteboardListener
+// UPDATE: This seems to be a bug in felix which should soon be resolved
+// https://issues.apache.org/jira/browse/FELIX-6049
+// As a workaround: don't use @HttpWhiteboardListener but
+// @Component(property= {HttpWhiteboardConstants.HTTP_WHITEBOARD_LISTENER + "=true"})
+
+@Component(property= {HttpWhiteboardConstants.HTTP_WHITEBOARD_LISTENER + "=true"})
 public class FlowOsgiRouteRegistryInitializer implements ServletContextListener {
 	
 	@Reference
